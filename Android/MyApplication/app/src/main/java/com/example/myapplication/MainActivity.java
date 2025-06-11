044cbd50fd299eec031a1367e1b29d989b4aa5a1
marco.pham <marco.pham@bowkergarment.com.vn>
2025-06-11 22:08:52 +0700

    Menu

--- Android/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,44 +1,58 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    Button button;
-    EditText username;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.ic_launcher_foreground));
+        setSupportActionBar(toolbar);
         ViewCompat.setOnApplyWindowInsetsListener(
                 findViewById(R.id.main),
                 (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
+                });
 
-        textView = findViewById(R.id.textView7);
-        button = findViewById(R.id.button);
-        username = findViewById(R.id.editTextText2);
+    }
 
-        button.setOnClickListener(view -> {
-            String content = username.getText().toString();
-            Toast.makeText(getApplicationContext(), content, Toast.LENGTH_LONG).show();
-            textView.setText(content);
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuSetting) {
+            Toast.makeText(this, "Menu Setting", Toast.LENGTH_LONG).show();
+        } else if (item.getItemId() == R.id.menuShare) {
+            Toast.makeText(this, "Menu Share", Toast.LENGTH_LONG).show();
+        } else if (item.getItemId() == R.id.menuSearch) {
+            Toast.makeText(this, "Menu Search", Toast.LENGTH_LONG).show();
+        } else if (item.getItemId() == R.id.menuEdit) {
+            Toast.makeText(this, "Menu Edit", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Unknow Menu", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
